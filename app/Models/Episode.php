2daa1e37d75c4b876f5502e4b2ea04e408546910
Francisco Cunha Neto <fcunhaneto@gmail.com>
2021-03-21 12:08:00 -0300

    MSCollection começa aqui.

---
+++ app/Models/Episode.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Episode extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'episodes';
+    public $timestamps = false;
+    protected $dates = ['deleted_at'];
+
+    public function seasons()
+    {
+        return $this->belongsTo(Season::class);
+    }
+}
---
+++ app/Models/Qualifiers/Keyword.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Qualifiers;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Keyword extends Model
+{
+    protected $table = 'keywords';
+    public $timestamps = false;
+    protected $dates = ['deleted_at'];
+}
